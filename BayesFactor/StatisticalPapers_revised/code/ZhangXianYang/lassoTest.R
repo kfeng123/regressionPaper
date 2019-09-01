cada9113c3a1ee7f27959ef592ab9478b78d7555
kfeng123 <446100240@qq.com>
2019-09-01 22:22:02 +0800

    NEAR

---
+++ BayesFactor/StatisticalPapers_revised/code/ZhangXianYang/lassoTest.R
@@ -0,0 +1,39 @@
+
+# X
+# y
+# set
+M <- 500
+here_alpha <- 0.95
+
+sreg <- scalreg(X, y)
+beta.hat <- sreg$coefficients
+sigma.sq <- sum((y-X%*%beta.hat)^2)/(n-sum(abs(beta.hat)>0))
+beta.db <- beta.hat+Theta%*%t(X)%*%(y-X%*%beta.hat)/n
+Omega <- diag(Theta%*%Gram%*%t(Theta))*sigma.sq
+stat.boot.st <- stat.boot.nst <- rep(NA,M)
+
+for (xianyang.i in 1:M) {
+    e <- rnorm(n)
+    xi.boot <- Theta[set,]%*%t(X)%*%e*sqrt(sigma.sq)/sqrt(n)
+    stat.boot.nst[xianyang.i] <- max(abs(xi.boot))
+    stat.boot.st[xianyang.i] <- max(abs(xi.boot)/sqrt(Omega[set]))
+}
+
+crit.nst <- quantile(stat.boot.nst, here_alpha)
+crit.st <- quantile(stat.boot.st, here_alpha)
+
+up.nst <- beta.db[set] + crit.nst/sqrt(n)
+low.nst <- beta.db[set] - crit.nst/sqrt(n)
+
+band.nst <- rbind(low.nst, up.nst)
+
+# test
+if(prod(up.nst > 0) * prod(low.nst < 0)){
+    # accept
+    lassoTest_one_result <- 0
+}else{
+    # reject
+    lassoTest_one_result <- 1
+}
+
+
--- BayesFactor/StatisticalPapers_revised/code/lassoLike.R
+++ BayesFactor/StatisticalPapers_revised/code/lassoLike.R
@@ -1,14 +1,50 @@
 library(glmnet)
+library(scalreg)
 n <- 100
 p <- 1000
 
 X <- rnorm(n*p); dim(X) <- c(n,p)
-y <- rnorm(n)
-lasso_compute <- glmnet(X, y,
+beta <- rep(1,p)
+#beta[sample(p,p-5, replace = TRUE)] <- 0
+y <- rnorm(n,0,2)  
+haha <- scalreg(X,y)
+
+lasso_model <- glmnet(X, y,
                         family="gaussian",
                         alpha=1,
                         nlambda = 1000,
                         standardize= FALSE,
                         intercept=FALSE)
-theCoef <- coef(lasso_compute, s = 0.1)
-thePred <- predict(lasso_compute, X, s=0.1)
+theCoef <- coef(lasso_model, s = 0.1)
+thePred <- predict(lasso_model, X, s=0.1)
+
+
+############ Scaled LASSO ########################
+SL.iterTime <- 1000
+SL.lambda_array <- rep(0, SL.iterTime)
+SL.lambda_array[1] <- 0.00001
+SL.sigma_array <- rep(0, SL.iterTime)
+for(i in 2:SL.iterTime){
+    SL.sigma_est <- sqrt( mean( (y - predict(lasso_model, X, s= SL.lambda_array[i-1]) )^2 ) )
+    SL.lambda_array[i] <- SL.lambda_array[i-1] * SL.sigma_est
+    SL.sigma_array[i] <- SL.sigma_est
+}
+
+
+
+############ Code provided by Xianyang Zhang
+
+n <- 50
+p <- 300
+
+X <- rnorm(n*p); dim(X) <- c(n,p)
+beta <- rep(1,p)
+#beta[sample(p,p-5, replace = TRUE)] <- 0
+set <- 1:5
+source("./ZhangXianYang/covarianceMatrix.R")
+lassoTest <- rep(0,1000)
+for(i in 1:1000){
+    y <- rnorm(n,0,2)  
+    source("./ZhangXianYang/lassoTest.R")
+    lassoTest[i] <- lassoTest_one_result
+}
